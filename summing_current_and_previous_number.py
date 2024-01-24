7074fe07e2931db057e0a99a10180fd2a3f98997
unknown <emgcalangian@gmail.com>
2024-01-24 21:18:46 +0800

    pseudo code

---
+++ summing_current_and_previous_number.py
@@ -0,0 +1,9 @@
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# pseudo code
+
+# create a separate value to add
+
+# use for loop to write the main code for summing
+
+# do adjustments
