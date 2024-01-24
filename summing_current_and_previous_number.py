3d0b06040af72a7bf0f9a184977ace17b704d3b8
unknown <emgcalangian@gmail.com>
2024-01-24 21:37:01 +0800

    final adjustments done

--- summing_current_and_previous_number.py
+++ summing_current_and_previous_number.py
@@ -3,13 +3,12 @@
 # pseudo code
 
 # create a separate value to add
-for_addition = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for_addition = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # use for loop to write the main code for summing
-for i in range(9):
+for i in range(10):
     current_number = for_addition[i + 1]
     previous_number = for_addition[i]
     the_sum = current_number + previous_number
-    print("The Current Number:", current_number, " The Previous Number:", previous_number, " Sum:", the_sum)
+    print("The Current Number:", current_number, "  The Previous Number:", previous_number, "  Sum:", the_sum)
 
 
-# do adjustments
